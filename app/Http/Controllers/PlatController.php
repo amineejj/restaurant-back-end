2f713ab548f039717ca85a1a663099525aa83ab6
amine ejj <amins8raja@gmail.com>
2021-06-11 20:22:55 +0100

    models and crud plat controller

---
+++ app/Http/Controllers/PlatController.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Plat;
+use Illuminate\Http\Request;
+
+class PlatController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return Plat::all();
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+            'nom' => 'required',
+            'description' => 'required',
+            'image' => 'required',
+            'prix' => 'required'
+        ]);
+        return Plat::create($request->all());
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        return Plat::find($id);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $plat = Plat::find($id);
+        return $plat->update($request->all());
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        return Plat::destroy($id);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  string  $nom
+     * @return \Illuminate\Http\Response
+     */
+    public function search($name)
+    {
+        return Plat::where('nom', 'like', '%'. $name .'%')->get();
+    }
+}
---
+++ database/migrations/2021_06_08_134456_create_commandes_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCommandesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('commandes', function (Blueprint $table) {
+            $table->id();
+            $table->double('prixTotal', 5, 2);
+            $table->string('adresse');
+            $table->string('livreur');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('commandes');
+    }
+}
